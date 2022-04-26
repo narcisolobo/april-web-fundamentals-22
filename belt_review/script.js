03456aeb6cd78ecbe8e32a4d6c357d306a974c61
narcisolobo <narcisolobo@gmail.com>
2022-04-26 18:54:06 -0500

    added belt review part 2

---
+++ belt_review/script.js
@@ -0,0 +1,11 @@
+function nextImg(element) {
+  element.src = "./images/pixel-ninjas-2.png";
+}
+
+function prevImg(element) {
+  element.src = "./images/stonepunk.png";
+}
+
+function increase(element) {
+  element.innerText++;
+}
